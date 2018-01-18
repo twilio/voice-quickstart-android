887551d21d6ba84e9d06464c813f168a2312c20b
Kumkum Bagchi <kbagchi@twilio.com>
2018-01-18 00:12:09 -0800

    Added skeleton of VoiceConnecionService

--- exampleconnectionservice/src/main/java/com/twilio/voice/exampleconnectionservice/VoiceActivity.java
+++ exampleconnectionservice/src/main/java/com/twilio/voice/exampleconnectionservice/VoiceActivity.java
@@ -1,8 +1,14 @@
 package com.twilio.voice.exampleconnectionservice;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,11 +16,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class VoiceActivity extends AppCompatActivity {
+    private static final String TAG = "VoiceActivity";
+
+    /*
+    * You must provide a Twilio Access Token to connect to the Voice service
+    */
+    private static final String TWILIO_ACCESS_TOKEN = "TWILIO_ACCESS_TOKEN";
+
+    private static final int MIC_PERMISSION_REQUEST_CODE = 1;
+    private static final int SNACKBAR_DURATION = 4000;
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice);
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +43,13 @@ public class VoiceActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        /*
+         * Ensure the microphone permission is enabled
+         */
+        if (!checkPermissionForMicrophone()) {
+            requestPermissionForMicrophone();
+        }
     }
 
     @Override
@@ -48,5 +72,39 @@ public class VoiceActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean checkPermissionForMicrophone() {
+        int resultMic = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        return resultMic == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissionForMicrophone() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+            Snackbar.make(coordinatorLayout,
+                    "Microphone permissions needed. Please allow in your application settings.",
+                    SNACKBAR_DURATION).show();
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    MIC_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        /*
+         * Check if microphone permissions is granted
+         */
+        if (requestCode == MIC_PERMISSION_REQUEST_CODE && permissions.length > 0) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(coordinatorLayout,
+                        "Microphone permissions needed. Please allow in your application settings.",
+                        SNACKBAR_DURATION).show();
+            } else {
+
+            }
+        }
     }
 }
---
+++ exampleconnectionservice/src/main/java/com/twilio/voice/exampleconnectionservice/VoiceConnectionService.java
@@ -0,0 +1,7 @@
+package com.twilio.voice.exampleconnectionservice;
+
+import android.telecom.ConnectionService;
+
+public class VoiceConnectionService extends ConnectionService {
+
+}
