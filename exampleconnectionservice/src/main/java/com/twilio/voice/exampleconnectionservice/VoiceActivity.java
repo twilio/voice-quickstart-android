2e3c8710e905150541d0303c0243cfa0d43806a1
Kumkum Bagchi <kbagchi@twilio.com>
2018-01-19 12:07:24 -0800

    making call works from the dialer and the app.

--- exampleconnectionservice/src/main/java/com/twilio/voice/exampleconnectionservice/VoiceActivity.java
+++ exampleconnectionservice/src/main/java/com/twilio/voice/exampleconnectionservice/VoiceActivity.java
@@ -1,19 +1,46 @@
 package com.twilio.voice.exampleconnectionservice;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.telecom.Connection;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telecom.ConnectionService;
+import android.telecom.DisconnectCause;
+import android.telecom.PhoneAccount;
+import android.telecom.PhoneAccountHandle;
+import android.telecom.TelecomManager;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.twilio.voice.Call;
+import com.twilio.voice.CallException;
+import com.twilio.voice.Voice;
+
+import java.util.HashMap;
+
+import static java.util.ResourceBundle.getBundle;
+
 
 public class VoiceActivity extends AppCompatActivity {
     private static final String TAG = "VoiceActivity";
@@ -21,11 +48,33 @@ public class VoiceActivity extends AppCompatActivity {
     /*
     * You must provide a Twilio Access Token to connect to the Voice service
     */
-    private static final String TWILIO_ACCESS_TOKEN = "TWILIO_ACCESS_TOKEN";
+    private static final String TWILIO_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJpc3MiOiJTS2QwMjkwNWY0OTNiMzNiNTBmZDNlMzJmYmRmNDMyMTIxIiwiZ3JhbnRzIjp7InZvaWNlIjp7Im91dGdvaW5nIjp7ImFwcGxpY2F0aW9uX3NpZCI6IkFQMTk4ZmE1ZjE1NTQxMWNiOTY2NDkwMWU2YTczYWVmNTgifSwicHVzaF9jcmVkZW50aWFsX3NpZCI6IkNSMWE3NWMyYzY4YTA3ZGMzZGFiMmMxZDEzOTYzNDlmYzUifSwiaWRlbnRpdHkiOiJrdW1rdW0ifSwic3ViIjoiQUM5NmNjYzkwNDc1M2IzMzY0ZjI0MjExZThkOTc0NmE5MyIsImV4cCI6MTUxNjM5NTk4MywibmJmIjoxNTE2MzA5NTgzfQ.nmojecobX1ulPzdqNmSTnkZaeY25MdVIylSFmBgtahM";
+
+    public static final String OUTGOING_CALL_ADDRESS = "OUTGOING_CALL_ADDRESS";
+
+    public static final String ACTION_OUTGOING_CALL = "ACTION_OUTGOING_CALL";
+    public static final String ACTION_DISCONNECT_CALL = "ACTION_DISCONNECT_CALL";
+    public static final String ACTION_REJECT_CALL = "ACTION_REJECT_CALL";
+    public static final String ACTION_DTMF_SEND = "ACTION_DTMF_SEND";
 
     private static final int MIC_PERMISSION_REQUEST_CODE = 1;
+    private static final int CALL_PHONE_CODE = 2;
     private static final int SNACKBAR_DURATION = 4000;
     private CoordinatorLayout coordinatorLayout;
+
+    private boolean isReceiverRegistered = false;
+    private VoiceBroadcastReceiver voiceBroadcastReceiver;
+
+    int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {Manifest.permission.RECORD_AUDIO, Manifest.permission.CALL_PHONE};
+
+    private TelecomManager telecomManager;
+    private PhoneAccountHandle handle;
+    private PhoneAccount phoneAccount;
+
+    HashMap<String, String> twiMLParams = new HashMap<>();
+    private AlertDialog alertDialog;
+    Call activeCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +88,24 @@ public class VoiceActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                alertDialog = createCallDialog(callClickListener(), cancelCallClickListener(), VoiceActivity.this);
+                alertDialog.show();
             }
         });
 
         /*
-         * Ensure the microphone permission is enabled
+         * Ensure the microphone and BIND_TELECOM_CONNECTION_SERVICE permissions are enabled
          */
-        if (!checkPermissionForMicrophone()) {
-            requestPermissionForMicrophone();
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        } else {
+            setupPhoneAccount();
+            /*
+             * Setup the broadcast receiver to be notified of FCM Token updates
+             * or incoming call invite in this Activity.
+             */
+            voiceBroadcastReceiver = new VoiceBroadcastReceiver();
+            registerReceiver();
         }
     }
 
@@ -74,29 +131,31 @@ public class VoiceActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private boolean checkPermissionForMicrophone() {
-        int resultMic = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
-        return resultMic == PackageManager.PERMISSION_GRANTED;
+    // Setup
+    void setupPhoneAccount() {
+        String appName = this.getString(R.string.app_name);
+        handle = new PhoneAccountHandle(new ComponentName(this.getApplicationContext(), VoiceConnectionService.class), appName);
+        telecomManager = (TelecomManager) this.getApplicationContext().getSystemService(this.getApplicationContext().TELECOM_SERVICE);
+        phoneAccount = new PhoneAccount.Builder(handle, appName)
+                .setCapabilities(PhoneAccount.CAPABILITY_CALL_PROVIDER)
+                .build();
+        telecomManager.registerPhoneAccount(phoneAccount);
     }
 
-    private void requestPermissionForMicrophone() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
-            Snackbar.make(coordinatorLayout,
-                    "Microphone permissions needed. Please allow in your application settings.",
-                    SNACKBAR_DURATION).show();
-        } else {
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{Manifest.permission.RECORD_AUDIO},
-                    MIC_PERMISSION_REQUEST_CODE);
+    // Request required permissions
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
         }
+        return true;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        /*
-         * Check if microphone permissions is granted
-         */
         if (requestCode == MIC_PERMISSION_REQUEST_CODE && permissions.length > 0) {
             if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                 Snackbar.make(coordinatorLayout,
@@ -105,6 +164,177 @@ public class VoiceActivity extends AppCompatActivity {
             } else {
 
             }
+        } else if (requestCode == CALL_PHONE_CODE && permissions.length > 0) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(coordinatorLayout,
+                        "CALL_PHONE permissions needed. Please allow in your application settings.",
+                        SNACKBAR_DURATION).show();
+            } else {
+
+            }
         }
     }
+
+    //
+    private class VoiceBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(ACTION_OUTGOING_CALL)) {
+                /*
+                 * Handle the incoming call invite
+                 */
+                handleCallRequest(intent);
+            } else if (action.equals(ACTION_DISCONNECT_CALL)) {
+                /*
+                 * hangup the active call
+                 */
+                if (activeCall != null) {
+                    activeCall.disconnect();
+                }
+            } else if (action.equals(ACTION_DTMF_SEND)) {
+                /*
+                 * send DTMF
+                 */
+                if (activeCall != null) {
+                    String digits = intent.getStringExtra("DTMF");
+                    activeCall.sendDigits(digits);
+                }
+            }
+        }
+    }
+
+    private void registerReceiver() {
+        if (!isReceiverRegistered) {
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction(ACTION_OUTGOING_CALL);
+            intentFilter.addAction(ACTION_DISCONNECT_CALL);
+            intentFilter.addAction(ACTION_DTMF_SEND);
+            LocalBroadcastManager.getInstance(this).registerReceiver(
+                    voiceBroadcastReceiver, intentFilter);
+            isReceiverRegistered = true;
+        }
+    }
+
+    private Call.Listener callListener() {
+        return new Call.Listener() {
+            @Override
+            public void onConnectFailure(Call call, CallException error) {
+                Log.d(TAG, "Connect failure");
+                String message = String.format("Call Error: %d, %s", error.getErrorCode(), error.getMessage());
+                Log.e(TAG, message);
+                Snackbar.make(coordinatorLayout, message, SNACKBAR_DURATION).show();
+                endCall();
+            }
+
+            @Override
+            public void onConnected(Call call) {
+                Log.d(TAG, "Connected");
+            }
+
+            @Override
+            public void onDisconnected(Call call, CallException error) {
+                Log.d(TAG, "Disconnected");
+                if (error != null) {
+                    String message = String.format("Call Error: %d, %s", error.getErrorCode(), error.getMessage());
+                    Log.e(TAG, message);
+                    Snackbar.make(coordinatorLayout, message, SNACKBAR_DURATION).show();
+                }
+                endCall();
+            }
+        };
+    }
+
+    private void handleCallRequest(Intent intent) {
+        if (intent != null && intent.getAction() != null) {
+            if (intent.getAction().equals(ACTION_OUTGOING_CALL)) {
+                String contact = intent.getStringExtra(VoiceActivity.OUTGOING_CALL_ADDRESS);
+                String[] contactparts = contact.split(":");
+                twiMLParams.put("to", contactparts[1]);
+                activeCall = Voice.call(VoiceActivity.this, TWILIO_ACCESS_TOKEN, twiMLParams, callListener());
+            }
+        }
+    }
+
+    private void makeCall(String to) {
+        Log.d(TAG, "makeCall");
+        Uri uri = Uri.fromParts("tel", to, null);
+        Bundle callInfoBundle = new Bundle();
+        callInfoBundle.putString("to", to);
+        Bundle callInfo = new Bundle();
+        callInfo.putParcelable(TelecomManager.EXTRA_OUTGOING_CALL_EXTRAS, callInfoBundle);
+        callInfo.putParcelable(TelecomManager.EXTRA_PHONE_ACCOUNT_HANDLE, handle);
+        callInfo.putBoolean(TelecomManager.EXTRA_START_CALL_WITH_VIDEO_STATE, true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        telecomManager.placeCall(uri, callInfo);
+    }
+
+    private void endCall() {
+        Log.d(TAG, "endCall");
+        Connection conn = VoiceConnectionService.getConnection();
+        if (conn == null) {
+            Snackbar.make(coordinatorLayout, "No call exists for you to end", SNACKBAR_DURATION).show();
+        } else {
+            DisconnectCause cause = new DisconnectCause(DisconnectCause.LOCAL);
+            conn.setDisconnected(cause);
+            conn.destroy();
+            VoiceConnectionService.deinitConnection();
+        }
+    }
+
+    private DialogInterface.OnClickListener callClickListener() {
+        return new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Place a call
+                EditText contact = (EditText) ((AlertDialog) dialog).findViewById(R.id.contact);
+                twiMLParams.put("to", contact.getText().toString());
+                // initiate the dialer
+                makeCall(contact.getText().toString());
+                alertDialog.dismiss();
+            }
+        };
+    }
+
+    private DialogInterface.OnClickListener cancelCallClickListener() {
+        return new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                alertDialog.dismiss();
+            }
+        };
+    }
+
+    public static AlertDialog createCallDialog(final DialogInterface.OnClickListener callClickListener,
+                                               final DialogInterface.OnClickListener cancelClickListener,
+                                               final Context context) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+        alertDialogBuilder.setIcon(R.drawable.ic_call_black_24dp);
+        alertDialogBuilder.setTitle("Call");
+        alertDialogBuilder.setPositiveButton("Call", callClickListener);
+        alertDialogBuilder.setNegativeButton("Cancel", cancelClickListener);
+        alertDialogBuilder.setCancelable(false);
+
+        LayoutInflater li = LayoutInflater.from(context);
+        View dialogView = li.inflate(R.layout.dialog_call, null);
+        final EditText contact = (EditText) dialogView.findViewById(R.id.contact);
+        contact.setHint(R.string.callee);
+        alertDialogBuilder.setView(dialogView);
+
+        return alertDialogBuilder.create();
+
+    }
+
 }
--- exampleconnectionservice/src/main/java/com/twilio/voice/exampleconnectionservice/VoiceConnectionService.java
+++ exampleconnectionservice/src/main/java/com/twilio/voice/exampleconnectionservice/VoiceConnectionService.java
@@ -1,7 +1,174 @@
 package com.twilio.voice.exampleconnectionservice;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.telecom.CallAudioState;
+import android.telecom.Connection;
+import android.telecom.ConnectionRequest;
 import android.telecom.ConnectionService;
+import android.telecom.DisconnectCause;
+import android.telecom.PhoneAccountHandle;
+import android.telecom.TelecomManager;
+import android.os.Handler;
+import android.widget.Toast;
+
+import static com.twilio.voice.exampleconnectionservice.VoiceActivity.ACTION_DISCONNECT_CALL;
+import static com.twilio.voice.exampleconnectionservice.VoiceActivity.ACTION_DTMF_SEND;
+import static com.twilio.voice.exampleconnectionservice.VoiceActivity.ACTION_OUTGOING_CALL;
 
 public class VoiceConnectionService extends ConnectionService {
+
+    private static String TAG = "VoiceConnectionService";
+    private static Connection mConnection;
+
+    public static Connection getConnection() {
+        return mConnection;
+    }
+
+    public static void deinitConnection() {
+        mConnection = null;
+    }
+
+    @Override
+    public Connection onCreateIncomingConnection(PhoneAccountHandle connectionManagerPhoneAccount, ConnectionRequest request) {
+
+        Toast.makeText(getApplicationContext(), "onCreateIncomingConnection called", Toast.LENGTH_SHORT).show();
+        Connection incomingCallCannection = createConnection(request);
+        incomingCallCannection.setRinging();
+        return incomingCallCannection;
+    }
+
+    @Override
+    public Connection onCreateOutgoingConnection(PhoneAccountHandle connectionManagerPhoneAccount, ConnectionRequest request) {
+        Toast.makeText(getApplicationContext(), "onCreateOutgoingConnection called", Toast.LENGTH_SHORT).show();
+
+        Connection outgoingCallConnection = createConnection(request);
+        outgoingCallConnection.setDialing();
+
+        return outgoingCallConnection;
+    }
+
+    private Connection createConnection(ConnectionRequest request) {
+        mConnection = new Connection() {
+            @Override
+            public void onStateChanged(int state) {
+                if(state == Connection.STATE_DIALING) {
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            sendCallRequestToActivity(ACTION_OUTGOING_CALL);
+                        }
+                    }, 100); // TODO decide on the delay
+                }
+            }
+
+            @Override
+            public void onCallAudioStateChanged (CallAudioState state) {
+                Toast.makeText(getApplicationContext(), "onCallAudioStateChanged called", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPlayDtmfTone (char c) {
+                Toast.makeText(getApplicationContext(), "onPlayDtmfTone called", Toast.LENGTH_SHORT).show();
+                Bundle extras = new Bundle();
+                extras.putString("DTMF", Character.toString(c));
+                mConnection.setExtras(extras);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendCallRequestToActivity(ACTION_DTMF_SEND);
+                    }
+                }, 100); // TODO decide on the delay
+            }
+
+            @Override
+            public void onDisconnect() {
+                super.onDisconnect();
+                mConnection.setDisconnected(new DisconnectCause(DisconnectCause.LOCAL));
+                mConnection.destroy();
+                mConnection = null;
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendCallRequestToActivity(ACTION_DISCONNECT_CALL);
+                    }
+                }, 100); // TODO decide on the delay
+            }
+
+            @Override
+            public void onSeparate() {
+                super.onSeparate();
+            }
+
+            @Override
+            public void onAbort() {
+                super.onAbort();
+                mConnection.setDisconnected(new DisconnectCause(DisconnectCause.CANCELED));
+                mConnection.destroy();
+            }
+
+            @Override
+            public void onHold() {
+                super.onHold();
+            }
+
+            @Override
+            public void onAnswer() {
+                super.onAnswer();
+                mConnection.setActive();
+            }
+
+            @Override
+            public void onReject() {
+                super.onReject();
+                mConnection.setDisconnected(new DisconnectCause(DisconnectCause.CANCELED));
+                mConnection.destroy();
+
+            }
+
+            @Override
+            public void onPostDialContinue(boolean proceed) {
+                super.onPostDialContinue(true);
+            }
+        };
+        mConnection.setAddress(request.getAddress(), TelecomManager.PRESENTATION_ALLOWED);
+        mConnection.setExtras(request.getExtras());
+        return mConnection;
+    }
+
+    /*
+     * Send the CallInvite to the VoiceActivity
+     */
+    private void sendCallRequestToActivity(String action) {
+        Intent intent = new Intent(action);
+        Bundle extras = new Bundle();
+        switch(action) {
+            case ACTION_OUTGOING_CALL:
+                Uri address = mConnection.getAddress();
+                extras.putString(VoiceActivity.OUTGOING_CALL_ADDRESS, address.toString());
+                intent.putExtras(extras);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+                break;
+            case ACTION_DISCONNECT_CALL:
+                extras.putInt("Reason", DisconnectCause.LOCAL);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+                break;
+            case ACTION_DTMF_SEND:
+                extras.putString("DTMF", mConnection.getExtras().getString("DTMF"));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                //LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+                break;
+            default:
+                break;
+        }
+    }
 
 }
